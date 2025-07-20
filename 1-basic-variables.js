eedc060a09a1ffd732d44c0e093deb43970f7f31
moss <mosscore.dev@gmail.com>
2025-07-20 22:43:47 +0500

    variables

---
+++ 1-basic-variables.js
@@ -0,0 +1,65 @@
+// basic variables
+
+let favioriteNumber = 8 // so i created a variable and gave it the number 10
+
+console.log(favioriteNumber)
+
+favioriteNumber = 10 // chaneged that variable to another number
+
+console.log(favioriteNumber)
+
+let a = 1
+let b = 2
+let c = 3 // created some more variables
+
+console.log(a, b, c)
+
+a = 5 // assisgned another number to one of the variable
+
+let d = a // assigned d is same as a
+
+console.log(d)
+
+// number type variable
+
+let int = 2 // number
+let float = 2.3 // floting type number (bsically means numbers with decimal places)
+
+console.log(typeof int)
+console.log(typeof float)
+
+console.log(int + float)
+console.log(1 + int)
+console.log(1 - int)
+console.log(2 * int)
+console.log(1 / int) // some basic variables
+
+// NOTE: this is the basics section of the js tutorial which i have already covered on learning-js repo. so if something is not new to me, i will just go about it very quick
+
+console.log(1 + 2 * 3)
+console.log((1 + 2) * 3)
+
+let e = 2
+let f = 5
+let g = 10
+
+console.log(e + f / g)
+console.log((e + f) * g)
+
+// string type variable
+
+let h = "Hello World"
+
+console.log(h)
+
+let i = 'Hello World'
+
+console.log(i) // ye you can do "" or ''
+
+console.log( typeof h)
+console.log(h + i)
+console.log(h +" " + i)
+
+let word = "moss"
+
+console.log("Hello my name is: " + word)
